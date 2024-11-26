37faed9140597d380378bedcd23ddb678c8a601b
AtharvaisOp <atharvaghuge2006@gmail.com>
2024-11-27 00:23:40 +0530

    completed

--- EXP9/scripts.js
+++ EXP9/scripts.js
@@ -1,4 +1,25 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const content = document.getElementById("content");
-    content.style.opacity = 1;
-});
+// For each progress bar, we will have a different start and end value
+
+function animateProgress(skillClass, startValue, endValue, speed) {
+  let circularProgress = document.querySelector(`.${skillClass}`),
+      progressValue = circularProgress.querySelector(".progress-value");
+
+  let progressStartValue = startValue;    
+
+  let progress = setInterval(() => {
+      progressStartValue++;
+      progressValue.textContent = `${progressStartValue}%`;
+      circularProgress.style.background = `conic-gradient(#7d2ae8 ${progressStartValue * 3.6}deg, #ededed 0deg)`;
+      if (progressStartValue == endValue) {
+          clearInterval(progress);
+      }
+  }, speed);
+}
+
+// Call the function for each skill with different values
+animateProgress("html", 0, 70, 70);  
+animateProgress("css", 0, 50, 70);  
+animateProgress("js", 0, 30, 70);
+animateProgress("C", 0, 90, 70);
+
+
